6c4fec1e25324faf328598dcb6351c32f647729b
olisheps <oli.shepherd@sociis.co.uk>
2020-04-21 11:39:12 +0100

    moved static files

--- PG/settings.py
+++ PG/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'website/static/website/')
+    os.path.join(BASE_DIR, 'website/static/')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
